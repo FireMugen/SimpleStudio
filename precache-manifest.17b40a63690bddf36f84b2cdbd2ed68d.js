02d48977e167523c24766b77c23a31ae4ca62620
Anthony <agdoueihi@gmail.com>
2019-05-16 19:54:47 +1000

    Updates

---
+++ precache-manifest.17b40a63690bddf36f84b2cdbd2ed68d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f7fd49a18516e31c28421f977ecd901",
+    "url": "/SimpleStudio/index.html"
+  },
+  {
+    "revision": "b263cac6347c589879aa",
+    "url": "/SimpleStudio/static/css/main.03891d97.chunk.css"
+  },
+  {
+    "revision": "3e6dcbced097d709db65",
+    "url": "/SimpleStudio/static/js/2.c67b7edc.chunk.js"
+  },
+  {
+    "revision": "b263cac6347c589879aa",
+    "url": "/SimpleStudio/static/js/main.1444b971.chunk.js"
+  },
+  {
+    "revision": "572efb3e25684d356df4",
+    "url": "/SimpleStudio/static/js/runtime~main.6d50b6b7.js"
+  },
+  {
+    "revision": "2b33869925bf363cd578c0d2e952c295",
+    "url": "/SimpleStudio/static/media/audio-mixer.2b338699.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7011320a9448224216cba992455e9dd1.js"
+  "/SimpleStudio/precache-manifest.17b40a63690bddf36f84b2cdbd2ed68d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SimpleStudio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
